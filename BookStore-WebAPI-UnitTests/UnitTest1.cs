e40a1204a68eff099881a2dacd719cfa7b87cb1a
ugurkiymetli <34272634+ugurkiymetli@users.noreply.github.com>
2021-12-11 17:53:34 +0300

    Test project added.

---
+++ BookStore-WebAPI-UnitTests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using Xunit;
+
+namespace BookStore_WebAPI_UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+        [Fact]
+        public void Test2()
+        {
+
+        }
+    }
+}
